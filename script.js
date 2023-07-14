e25a818aedcaa8c26d70714cafc83e18f9e98555
mesakhlolo <mesakhlolo12@gmail.com>
2023-07-14 20:31:17 +0700

    menyelesaikan logic script menggunakan eval()

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
 const calculate = (btnValue) => {
-  console.log(btnValue);
+  if (btnValue === "=" && output !== "") {
+    // if output has '%', replace with '/100' before evaluate
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    // if 'DEL' button click
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
 };
 
-// menambahkan event listener untuk buttons, panggil calculate() ketika diclick
+// event listener for buttons
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => console.log(e.target.dataset.value));
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
